e2d9735b20ffd57d1988fecc264b66e557f73833
sky-pixels <hannah.kim1324@gmail.com>
2024-05-16 20:56:59 -0700

    finished graph

--- chart_Hannah.R
+++ chart_Hannah.R
@@ -1,7 +1,21 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
+library(dplyr)
 library(ggplot2)
-ggplot() +
-  geom_point(aes(x=x_values, y = y_values))
 
+df <- read.csv('icecream_data.csv')
+
+classify_ice_cream <- function(description) {
+  if (grepl('dairy-free|non-dairy|sorbetto', description, ignore.case = TRUE)) {
+    return('dairy-free')
+  } else {
+    return('dairy')
+  }
+}
+
+df$category <- sapply(df$description, classify_ice_cream)
+
+ggplot(df, aes(x = category, y = rating, fill = category)) +
+  geom_boxplot() +
+  scale_fill_manual(values = c("dairy" = "#fca9c9", "dairy-free" = "#b0f7f1")) +
+  labs(title = 'Distribution of Ratings for Dairy and Dairy-Free Ice Creams',
+       x = 'Category', y = 'Rating') +
+  theme_minimal()
